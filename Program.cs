2d24f87bc5e8dce7297033d04a1b91911b9a2131
KaleTheNoise <zwisner@rategenius.com>
2019-07-29 14:05:48 -0500

    starting the Game of Life program

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace gameoflife
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public static int[,] deadState(int width, int height)
+        {
+            int [,] theBoard = new int[width,height];
+            
+        }
+    }
+}
