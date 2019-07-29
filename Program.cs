f923c86141be87a0e28ae1fe06156f826be992d9
KaleTheNoise <zwisner@rategenius.com>
2019-07-29 16:32:09 -0500

    got dead board generating properly

--- Program.cs
+++ Program.cs
@@ -4,14 +4,34 @@ namespace gameoflife
 {
     class Program
     {
+        public static int DEAD = 0;
+        public static int LIVE = 1;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //test print dead board
+            int[,] board = deadState(4,4);
+            for (int i=0;i<board.GetLength(0); i++)
+            {
+                for (int j=0; j<board.GetLength(1); j++)
+                {
+                    Console.Write(board[i,j] +"\t");
+                }
+                Console.WriteLine();
+            }
+            
         }
         public static int[,] deadState(int width, int height)
         {
-            int [,] theBoard = new int[width,height];
-            
+            int[,] theBoard = new int[width, height];
+            for(int i=0; i< width; i++)
+            {
+                for (int j=0; j<height; j++)
+                {
+                    theBoard[i,j]=DEAD;
+                }
+
+            }
+            return theBoard;
         }
     }
 }
