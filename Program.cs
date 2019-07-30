f26ab681af9ad31d8ff9e697d0d7dd9f31460d05
KaleTheNoise <zwisner@rategenius.com>
2019-07-30 15:01:27 -0500

    Generating random boards prints nicer

--- Program.cs
+++ Program.cs
@@ -8,19 +8,49 @@ namespace gameoflife
         public static int LIVE = 1;
         static void Main(string[] args)
         {
-            //test print dead board
-            int[,] board = deadState(4,4);
+            int[,] board = randomState(5,10);
+            renderBoard(board);                        
+        }
+      
+        //generate a randomized starting board
+        public static int[,] randomState(int width, int height)
+        {
+            int[,] theBoard = new int[width, height];
+            Random rand = new Random();
+            for(int i=0; i< width; i++)
+            {
+                for (int j=0; j<height; j++)
+                {
+                    if (rand.NextDouble() >= 0.5)
+                        theBoard[i,j]=LIVE;
+                    else
+                        theBoard[i,j]=DEAD;
+                }
+
+            }
+            return theBoard;
+        }
+
+        public static void renderBoard(int[,] board)
+        {
+            for (int tL=0; tL<=board.GetLength(1)+1;tL++)
+                Console.Write("_");
+                Console.WriteLine();
             for (int i=0;i<board.GetLength(0); i++)
             {
+                Console.Write("|");
                 for (int j=0; j<board.GetLength(1); j++)
                 {
-                    Console.Write(board[i,j] +"\t");
+                    if (board[i,j] == LIVE)
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
                 }
-                Console.WriteLine();
+                Console.WriteLine("|");
             }
-            
         }
-        public static int[,] deadState(int width, int height)
+        //generate a completely dead board
+          public static int[,] deadState(int width, int height)
         {
             int[,] theBoard = new int[width, height];
             for(int i=0; i< width; i++)
